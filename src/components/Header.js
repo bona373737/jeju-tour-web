c02d38a9d2d909ce3b08f22a2da2d77c60707e81
이재이 <loveleej87@gmail.com>
2022-06-14 16:44:13 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,8 @@ import { NavLink } from 'react-router-dom';
 import Sidebar from './Sidebar'; 
 import Search from './Search';
 
+import Logo from "../assets/icon/TRAY.png";
+
 
 const HeaderContainer = styled.div`
     box-sizing: border-box;
@@ -12,16 +14,29 @@ const HeaderContainer = styled.div`
     height: 8vh;
     background-color: var(--white);
 
-    .content-wrap{
+    .content-wrap {
         box-sizing: border-box;
         width: 100%;
-        height: 100%;
+        height: 60px;
         display: flex;
         justify-content: space-between;
         align-items: center;
-        .icons{
+        box-shadow: 0.5em;
+        background-color: #eee;
+
+        .icons {
             display: flex;
         }   
+
+        h1 {
+            text-indent: -9999em;
+        }
+
+        img {
+            width: 60px;
+            position: absolute;
+            left: 20px;
+        }
     }
 `;
 
@@ -29,18 +44,21 @@ const Header = () => {
     return (
         <HeaderContainer>
             <div className="content-wrap">
-                <NavLink to='/'>
+                <NavLink to="/">
+                    <img src={Logo} alt="Logo" />
                     <h1>TRAY</h1>
                 </NavLink>
                 <div className="icons">
-                    <h1>검색</h1>
+                    <h1>
+                        검색
+                    </h1>
                     <h1>☰</h1>
                 </div>
             </div>
-            
+
             {/*조건부 렌더링_검색,햄버거 토글*/}
             <Search></Search>
-            <Sidebar></Sidebar> 
+            <Sidebar></Sidebar>
         </HeaderContainer>
     );
 };
