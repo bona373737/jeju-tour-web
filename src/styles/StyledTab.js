ace2d19d192deae344846290257a736d5b733e78
NaraeGu <nrggrnngg@gmail.com>
2022-06-16 18:37:35 +0900

    TabButton, StyledTab 수정

--- src/styles/StyledTab.js
+++ src/styles/StyledTab.js
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 const StyledTabContainer = styled(NavLink)`
-
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
 `;
 
 const StyledTab = ({to,children}) => {
