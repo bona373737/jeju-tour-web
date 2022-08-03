6efc02e652587d45dac9eda5dad4bbec8b9ba925
bona <bona373737@gmail.com>
2022-08-04 01:55:07 +0900

    회원가입페이지 비밀번호암호화 코드 추가_bcryptjs모듈설치

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -10,7 +10,7 @@ import styled from "styled-components";
 import RegexHelper from '../libs/RegexHelper';
 import useAxios from "axios-hooks";
 
-// import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 // import Arrow from "../assets/icon/arrow.png";
 
@@ -37,17 +37,18 @@ const SignupContainer = styled.div`
             width: 100%;
 
             .message {
-                display: flex;
+                /* display: flex; */
                 padding-bottom: 2%;
+
+                span{
+                    font-size: 14px;
+                    color: red;
+
+                }
             }
 
             label {
                 font-size: 14px;
-            }
-
-            p {
-                font-size: 14px;
-                color: red;
             }
 
             /* select 기본 스타일 제거 */
@@ -102,8 +103,11 @@ const SignupContainer = styled.div`
 
             .email_area {
                 width: 100%;
-                display: flex;
-                justify-content: space-between;
+                
+                .input_box{
+                    display: flex;
+                    justify-content: space-between;
+                }
 
                 .user_email {
                     width: 40%;
@@ -140,8 +144,7 @@ const Signup = () => {
     const month = [];
     for (let i = 1; i < 13; i++) month.push(i);
 
-    /**사용자 입력값 변수선언 */
-    let userid = null;
+    let userid = null
     let password = null;
     let passwordCheck = null;
     let username = null;
@@ -151,29 +154,44 @@ const Signup = () => {
     /**input입력칸 onBlur 이벤트 */
     const onBlur = useCallback((e)=>{
 
-        try {
-            RegexHelper.value(e.target.userid,'아이디를 입력해 주세요');
-            RegexHelper.engNum(e.target.userid,'아이디는 영어,숫자만 가능합니다.');
-            userid = e.target.userid;
-
-            RegexHelper.value(e.target.password,'비밀번호를 입력해 주세요');
-            password = e.target.password;
-            
-            RegexHelper.value(e.target.passwordCheck,'비밀번호 확인을 입력해 주세요');
-            passwordCheck = e.target.passwordCheck;
-
-            RegexHelper.value(e.target.username,'성함를 입력해 주세요');
-            username = e.target.username;
-
-            if(e.target.birthday){
-                RegexHelper.value(e.target.birthday,'생년월일을 입력해 주세요');
-                birthday = e.target.birthday;
+        try {            
+            switch (e.target.id) {
+                case 'userid':
+                    RegexHelper.value(e.target.value,'아이디를 입력해 주세요');
+                    RegexHelper.engNum(e.target.value,'아이디는 영어,숫자만 가능합니다.');
+                    break;
+                case 'password':
+                    RegexHelper.value(e.target.value,'비밀번호를 입력해 주세요');
+                    break;
+                    case 'passwordCheck':
+                    RegexHelper.value(e.target.value,'비밀번호 확인을 입력해 주세요');
+                    break;
+                case 'username':
+                    RegexHelper.value(e.target.value,'성함를 입력해 주세요');
+                    break;
+                case 'birth_year':
+                    if(e.target.value){
+                        RegexHelper.value(e.target.value,'출생년도를 입력해 주세요');
+                    }
+                    break;
+                case 'birth_month':
+                    if(e.target.value){
+                        RegexHelper.value(e.target.value,'출생 월을 선택해 주세요');
+                    }
+                    break;
+                case 'birth_day':
+                    if(e.target.value){
+                        RegexHelper.value(e.target.value,'출생 일자를 입력해 주세요');
+                    }
+                    break;
+                case 'email':
+                    RegexHelper.value(e.target.value, '이메일을 입력해주세요.');
+                    RegexHelper.engNum(e.target.value, '이메일은 영어,숫자만 가능합니다.')
+                    break;
+                default:
+                    break;
             }
-
-            RegexHelper.value(e.target.email, '이메일을 입력해주세요.');
-            RegexHelper.email(e.target.email,'이메일 형식이 잘못되었습니다.');
-            email = e.target.email;
-
+                    
         } catch (error) {
             console.log(error);
             throw error;
@@ -184,7 +202,7 @@ const Signup = () => {
     /**입력값 post전송함수 정의 axios-hooks 모듈사용  */
     const [{ data, loading, error }, refetch] = useAxios({
           url: 'http://localhost:3001/members',
-          method: 'PUT'
+          method: 'POST'
         },
         { manual: true })
 
@@ -192,37 +210,58 @@ const Signup = () => {
     const onSubmit=async(e)=>{
         e.preventDefault();
 
+        const current = e.target;
+        
+        userid = current.userid.value;
+        password = current.password.value;
+        passwordCheck = current.passwordCheck.value;
+        username = current.username.value;
+        birthday = current.birth_year.value+'-'+current.birth_month.value+'-'+current.birth_day.value;
+        email = current.input_email.value+current.input_domain.value
+        
+        /**유효성검사*/
         try {
-            RegexHelper.value(e.target.userid,'아이디를 입력해 주세요')
-            RegexHelper.engNum(e.target.userid,'아이디는 영어,숫자만 가능합니다.')
-            RegexHelper.value(e.target.password,'비밀번호를 입력해 주세요')            
-            RegexHelper.value(e.target.passwordCheck,'비밀번호 확인을 입력해 주세요')
-            RegexHelper.value(e.target.username,'성함를 입력해 주세요')
-            if(e.target.birthday){
-                RegexHelper.value(e.target.birthday,'생년월일을 입력해 주세요')
-                birthday = e.target.birthday;
+            RegexHelper.value(userid,'아이디를 입력해 주세요')
+            RegexHelper.engNum(userid,'아이디는 영어,숫자만 가능합니다.')
+            RegexHelper.value(password,'비밀번호를 입력해 주세요')            
+            RegexHelper.value(passwordCheck,'비밀번호 확인을 입력해 주세요')
+            RegexHelper.value(username,'성함를 입력해 주세요')
+            if(birthday){
+                RegexHelper.value(birthday,'생년월일을 입력해 주세요')
             }
-            RegexHelper.value(e.target.email, '이메일을 입력해주세요.')
-            RegexHelper.email(e.target.email,'이메일 형식이 잘못되었습니다.')
+            RegexHelper.value(email, '이메일을 입력해주세요.')
+            RegexHelper.email(email,'이메일 형식이 잘못되었습니다.')
 
         } catch (error) {
             console.log(error);
             throw error;
         }
 
-        /**비밀번호 암호화_bcrypt모듈 사용 */
+        /**비밀번호 암호화_bcryptjs모듈 사용 */
+        const salt = await bcrypt.genSalt(10);
+        password = await bcrypt.hash(password, salt);
 
+        /**유효성검사 완료 후 입력값 변수로 저장 */
+        const input_data={
+            userid : userid,
+            password : password,
+            passwordCheck: passwordCheck,
+            username : username,
+            birthday : birthday,
+            email : email
+        }
 
+        let json = null;
         try {
-            await refetch()
+            await refetch({data:input_data})
+            // json = response.data;
         } catch (error) {
-            
+            console.log(e);
+            // window.(`[ ${e.response.status} ] ${e.response.statusText} \n ${e.message}`);
         }
 
     };
 
-    
-    
 
     return (
         <SignupContainer>
@@ -232,155 +271,70 @@ const Signup = () => {
                 <form onSubmit={onSubmit}>
                     {/* 아이디 */}
                     <div>
-                        <div className="message">
-                            <label htmlFor="userid">아이디</label>&nbsp;<p>*</p>
-                        </div>
-                        <input
-                            id="userid"
-                            name="userid"
-                            type="text"
-                            className="input_text"
-                            onBlur={onBlur}
-                        ></input>
-                        <span id="err_id" className="err_msg">
-                            아이디를 입력하세요.
-                        </span>
+                        <label htmlFor="userid" className="message">아이디 <span>*</span></label>
+                        <input id="userid" name="userid" type="text" onBlur={onBlur} className="input_text" ></input>
+                        <span id="err_id" className="err_msg"> 아이디를 입력하세요. </span>
                         <br />
                     </div>
 
                     {/* 비밀번호 */}
-                    <div>
-                    <div className="message">
-                        <label htmlFor="passord">비밀번호</label>&nbsp;<p>*</p>
-                    </div>
-                    <input
-                        id="passord"
-                        name="passord"
-                        type="password"
-                        className="input_text"
-                        placeholder="영문+숫자 조합 8~16자리"
-                    ></input>
-                    <span id="err_pw" className="err_msg">
-                        비밀번호를 입력하세요.
-                    </span>
+                    <div >
+                    <label htmlFor="passord" className="message">비밀번호 <span>*</span></label>
+                    <input id="password" name="password" type="password" onBlur={onBlur} className="input_text" placeholder="영문+숫자 조합8~16자리"></input>
+                    <span id="err_pw" className="err_msg"> 비밀번호를 입력하세요. </span>
                     <br />
                     </div>
 
                     {/* 비밀번호 확인 */}
                     <div>
-                        <div className="message">
-                            <label htmlFor="passordCheck">비밀번호 확인</label>
-                            &nbsp;<p>*</p>
-                        </div>
-                        <input
-                            id="passordCheck"
-                            name="passordCheck"
-                            type="password"
-                            className="input_text"
-                        ></input>
-                        <span id="err_pw_check" className="err_msg">
-                            비밀번호가 일치하지 않습니다.
-                        </span>
+                        <label htmlFor="passordCheck" className="message">비밀번호 확인<span>*</span></label>
+                        <input id="passwordCheck" name="passwordCheck" type="password" onBlur={onBlur} className="input_text"/>
+                        <span id="err_pw_check" className="err_msg"> 비밀번호가 일치하지 않습니다.</span>
                         <br />
                     </div>
 
                     {/* 이름 */}
                     <div>
-                    <div className="message">
-                        <label htmlFor="username">이름</label>&nbsp;<p>*</p>
-                    </div>
-                    <input
-                        id="username"
-                        name="username"
-                        type="text"
-                        className="input_text"
-                    ></input>
-                    <span id="err_name" className="err_msg">
-                        이름을 입력하세요.
-                    </span>
-                    <br />
+                        <label htmlFor="username" className="message">이름<span>*</span></label>
+                        <input id="username" name="username" type="text" className="input_text" onBlur={onBlur}></input>
+                        <span id="err_name" className="err_msg"> 이름을 입력하세요.</span>
+                        <br />
                     </div>
 
                     {/* 생년월일 */}
-                    <div className="message">
-                        <label htmlFor="user_yy">생년월일</label>
-                    </div>
-
                     <div>
-                    <div className="birth_area">
-                        <input
-                            id="user_yy"
-                            name="user_yy"
-                            type="text"
-                            placeholder="년(4자)"
-                            area-label="년(4자)"
-                            className="user_birth"
-                            maxLength={4}
-                        />
-
-                        <select 
-                            id="user_mm"
-                            name="user_mm"
-                            className="user_mm"
-                            area-label="월"
-                        >
-                            <option value="">월</option>
-                            {month.map((v, i) => {
-                                return (
-                                    <option value={v} key={i}>
-                                        {v}
-                                    </option>
-                                );
-                            })}
-                        </select>
-
-                        <input
-                            id="user_dd"
-                            name="user_dd"
-                            type="text"
-                            placeholder="일"
-                            area-label="일"
-                            className="user_birth"
-                            maxLength={2}
-                        />
-                    </div>
-
-                    <span id="err_birth" className="err_msg">
-                        생년월일을 입력하세요.
-                    </span>
-                    <br />
+                        <label htmlFor="birth_year" className="message">생년월일</label>
+                        <div className="birth_area">
+                            <input id="birth_year" name="birth_year" type="text" placeholder="년(4자)" className="user_birth" maxLength={4} onBlur={onBlur} />
+                            <select id="birth_month" name="birth_month" className="birth_month" placeholder="월" onBlur={onBlur}>
+                                <option value="">월</option>
+                                {month.map((v, i) => {
+                                    return (
+                                        <option value={v} key={i}>
+                                            {v}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                            <input id="birth_day" name="birth_day" type="text" placeholder="일" className="user_birth" maxLength={2} onBlur={onBlur}/>
+                        </div>
+                       <span id="err_birth" className="err_msg"> 생년월일을 입력하세요. </span>
+                        <br />
                     </div>
 
                     {/* 이메일 */}
-                    <div className="message">
-                        <label htmlFor="user_email">이메일</label>
-                    </div>
-
                     <div className="email_area">
-                        <input
-                            id="user_email"
-                            name="user_email"
-                            type="text"
-                            className="user_email"
-                            maxLength={20}
-                        />
-
-                        <select
-                            id="user_email"
-                            name="user_email"
-                            type="text"
-                            className="user_email_sel"
-                            placeholder="@gmail.com"
-                        >
-                            <option value="gmail">@gmail.com</option>
-                            <option value="gmail">@naver.com</option>
-                            <option value="gmail">@daum.com</option>
-                        </select>
+                        <label htmlFor="user_email" className="message">이메일</label>
+                        <div className="input_box">
+                            <input id="input_email" name="input_email" type="text"  onBlur={onBlur} className="user_email" maxLength={20}/>
+                            <select id="input_domain" name="input_domain" type="text" className="user_email_sel">
+                                <option value="@gmail.com">@gmail.com</option>
+                                <option value="@naver.com">@naver.com</option>
+                                <option value="@daum.com">@daum.com</option>
+                            </select>
+                        </div>
                     </div>
-
-                    <span id="err_email" className="err_msg">
-                        이메일 주소를 입력하세요.
-                    </span>
+                    <span id="err_email" className="err_msg"> 이메일 주소를 입력하세요.</span>
                     <br />
 
                     <button type="submit" id="signup" className="signup">
