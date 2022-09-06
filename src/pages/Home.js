7c8c5f885a0d52deba386c0cd19bccf3d693397a
NaraeGu <nrggrnngg@gmail.com>
2022-09-07 00:29:00 +0900

    메인 홈페이지 리스트, 상세 페이지 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,47 @@
 /** 
  * @Filename: Home.js
- * @Author: 이재이(loveleej87@gmail.com)
- * @Description: 홈 페이지
+ * @Author: 웹퍼블작업_이재이(loveleej87@gmail.com)
+ *          js기능구현_구나래(nrggrnngg@gmail.com)
+ * @Description: 홈 페이지 추천 테마 리스트
+ *               최신 여행지 정보 조회 후 리스트로 출력
  */
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { getPlaceList } from "../slices/PlaceSlice";
 
+import Spinner from '../components/Spinner';
 import ThemeItem from "../components/items/ThemeItem";
 import TabArea from "../components/TabArea";
 
 const HomeContainer = styled.div`
     width: 100%;
     height: 100%;
+    padding-bottom: 10%;
     background-color: #e3eeff88;
 `;
 
 const Home = () => {
+    // redux 최신 여행지 정보 조회
+    const dispatch = useDispatch();
+    const { data, loading, error } = useSelector((state) => state.place);
+
+    // 페이지 마운트 시 여행지 정보 확인
+    useEffect(() => {
+        dispatch(getPlaceList({
+            rows: 5
+        }));
+    },[dispatch]);
+
     return (
         <HomeContainer>
             <TabArea />
-            {/* map사용하여 ThemeItem 반복으로 출력하기 */}
-            <ThemeItem></ThemeItem>
-            <ThemeItem></ThemeItem>
-            <ThemeItem></ThemeItem>
-            <ThemeItem></ThemeItem>
+
+            <Spinner visible={loading} />
+
+            {data && (
+                data.item.map((v, i) => <ThemeItem key={v.place_no} item={v}/>)
+            )}
         </HomeContainer>
     );
 };
