a76161a9971ec8c9e633b85e16a6e5e6b6fc1110
NaraeGu <nrggrnngg@gmail.com>
2022-09-07 18:50:20 +0900

    로그인 유효성 검사 수정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,7 +5,7 @@
  * @Description: 회원 로그인 페이지
  *               비밀번호 암호화_crypto-js사용_양방향암호화하여 전송
  */
-import React, { useCallback } from "react";
+import React, { useCallback, useRef } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import crypto from 'crypto-js';
@@ -41,24 +41,22 @@ const LoginContainer = styled.div`
                 width: 90%;
                 height: 16px;
                 padding: 5%;
+
+                &:first-child {
+                    margin-bottom: 3%;
+                }
             }
 
             .login {
-                margin-top: 10%;
+                margin-top: 7%;
             }
 
             .err_msg {
                 color: red;
-                display: flex;
                 font-size: 14px;
-                padding: 4% 0 4% 0;
-                margin-bottom: 2%;
+                padding-top: 4%;
+                line-height: 1.5;
             }
-        }
-
-        p {
-            color: red;
-            padding-top: 4%;
         }
     }
 
@@ -68,7 +66,7 @@ const LoginContainer = styled.div`
         justify-content: space-between;
 
         .find_btn {
-            margin: 5% 0;
+            margin: 7% 0;
             width: 48%;
         }
     }
@@ -80,27 +78,8 @@ const Login = () => {
     // 리덕스 로그인 세션 상태 관리
     const dispatch = useDispatch();
     const { loading } = useSelector((state) => state.member);
-
-    /** input 입력칸 onBlur 이벤트 */
-    const onBlur = useCallback(e => {
-        // 입력값에 대한 유효성 검사
-        try {
-            switch(e.target.name) {
-                case 'userid':
-                    regexHelper.value(e.target.value, '아이디를 입력해주세요.');
-                    regexHelper.engNum(e.target.value,'아이디는 영어, 숫자만 입력 가능합니다.');
-                break;
-                case 'password':
-                    regexHelper.value(e.target.value, '비밀번호를 입력해주세요');
-                break;
-                default:
-                break;
-            }
-        } catch (err) {
-            console.log(err);
-            throw err;
-        }
-    }, []);
+    // 에러 메시지 참조변수
+    const errMsgRef = useRef();
 
     /** form submit 이벤트 */
     const loginSubmit = useCallback(e => {
@@ -111,12 +90,22 @@ const Login = () => {
         const userid = current.userid.value;
         let password = current.password.value;
 
+        // 아이디, 비밀번호 유효성 검사
+        try { 
+            regexHelper.value(userid, '아이디를 입력해주세요.');
+            regexHelper.value(password, '비밀번호를 입력해주세요.');
+        } catch(err) {
+            // 로그인 실패 시
+            errMsgRef.current.innerHTML = err.message;
+            err.message === '아이디를 입력해주세요.' ? current.userid.focus() : current.password.focus();
+            return;
+        }
+
         /** 비밀번호 암호화_crypto-js모듈 사용 */
         // AES알고리즘 사용 --> 사용자 입력값 암호화
         const secretKey = process.env.REACT_APP_CRYPTO_KEY;
         password = crypto.AES.encrypt(password, secretKey).toString();
     
-
         // 리덕스를 통해 로그인 요청
         dispatch(postLogin({
             userid: userid,
@@ -129,7 +118,7 @@ const Login = () => {
         })
         .catch(err => {
             // 로그인 실패 시
-            alert(err.data.rtmsg);
+            errMsgRef.current.innerHTML = err.data.rtmsg;
         });
     }, [dispatch, navigate]);
 
@@ -141,13 +130,10 @@ const Login = () => {
                 <div className="login_content">
                     <h3 className="font2">로그인</h3>
                     <form onSubmit={loginSubmit}>
-                        <input type="text" name="userid" onBlur={onBlur} className="input_text" placeholder="아이디"></input>
-                        <span id="err_id" className="err_msg">아이디를 입력하세요.</span>
-
-                        <input type="password" name="password" onBlur={onBlur} className="input_text" placeholder="비밀번호"></input>
-                        <span id="err_pw" className="err_msg">비밀번호를 입력하세요.</span>
-
-                        <button type="submit" name="login" className="btn_act">로그인</button>
+                        <input type="text" name="userid" className="input_text" placeholder="아이디"></input>
+                        <input type="password" name="password" className="input_text" placeholder="비밀번호"></input>
+                        <div id="err_msg" className="err_msg" ref={errMsgRef}></div>
+                        <button type="submit" name="login" className="login btn_act">로그인</button>
                     </form>
 
                     <div className="find_btn_area">
